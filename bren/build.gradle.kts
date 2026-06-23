7427ae57e8d11d004a7fb4150491e3542111b85c
Mika <nicholas.irish@gmail.com>
2026-06-23 07:39:04 +0100

    Added DB support

--- bren/build.gradle.kts
+++ bren/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.3.10"
+    kotlin("plugin.jpa") version "2.4.0"
+
 }
 
 group = "org.iglossolalia"
@@ -7,14 +9,17 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-web:4.1.0")
+    implementation("com.h2database:h2:2.4.240")
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(25)
 }
 
 tasks.test {
