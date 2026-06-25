c149b8487801986279c6117bd580ca8a2972d67a
Mika <nicholas.irish@gmail.com>
2026-06-25 08:42:57 +0100

    External routing support

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.tsx'
-import {BrowserRouter} from "react-router";
+import {HashRouter} from "react-router";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <BrowserRouter><App /></BrowserRouter>
+      <HashRouter><App /></HashRouter>
   </StrictMode>,
 )
